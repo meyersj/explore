39c7c1b51cd61329876f956f6f79b6088459b5b1
Jeffrey Meyers <meyers2@pdx.edu>
2015-10-14 11:30:56 -0700

    initial commit for android client

---
+++ android-client/app/src/main/java/com/meyersj/tracker/BluetoothFragment.java
@@ -0,0 +1,75 @@
+package com.meyersj.tracker;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class BluetoothFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private final String TAG = getClass().getCanonicalName();
+
+    //@Bind(R.id.active_list) ListView activeListView;
+    @Bind(R.id.start_button) Button startButton;
+    @Bind(R.id.stop_button) Button stopButton;
+
+    private BluetoothScanner scanner;
+
+    public static BluetoothFragment newInstance(int sectionNumber) {
+        BluetoothFragment fragment = new BluetoothFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public BluetoothFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, rootView);
+
+        scanner = new BluetoothScanner();
+
+        // add START and STOP scan button listeners
+        setViewListeners();
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        ((MainActivity) getActivity()).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    private void setViewListeners() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Start Discovery", Toast.LENGTH_SHORT).show();
+                scanner.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Stop Discovery", Toast.LENGTH_SHORT).show();
+                scanner.stop();
+            }
+        });
+    }
+
+}
