65f4c66194b97c53b34395ff50220d39c6a96dbb
Jeffrey Meyers <meyers2@pdx.edu>
2015-10-20 17:14:35 -0700

    add more to adv test

--- go-server/payload/advertisement_test.go
+++ go-server/payload/advertisement_test.go
@@ -33,9 +33,6 @@ func Test_InitAdvertisement_valid2(t *testing.T) {
 	data := []byte{100, 3, 1, 0, 0, 3, 2, 0, 0}
 
 	adv := InitAdvertisement(data)
-	if adv.Rssi != 100 {
-		t.Fatalf("RSSI should be 100")
-	}
 	if len(adv.Structures) != 2 {
 		t.Fatalf("Length of structures does not equal 3")
 	}
@@ -47,13 +44,29 @@ func Test_InitAdvertisement_valid2(t *testing.T) {
 	}
 }
 
+func Test_InitAdvertisement_valid3(t *testing.T) {
+	data := []byte{100, 4, 1, 0, 0, 0, 2, 2, 0, 5, 3, 0, 0, 0, 0}
+
+	adv := InitAdvertisement(data)
+	if len(adv.Structures) != 3 {
+		t.Fatalf("Length of structures does not equal 3")
+	}
+	if adv.Structures[0].Type != 0x01 {
+		t.Fatalf("Failed to parse Type=0x01 for first Structure")
+	}
+	if len(adv.Structures[1].Data) != 1 && adv.Structures[1].Type != 0x02 {
+		t.Fatalf("Failed to parse Type=0x03 for first Structure")
+	}
+}
+
 func Test_InitAdvertisement_invalid(t *testing.T) {
 	data := []byte{100, 5, 1, 0, 0, 0} // length of data does not match length field
 	adv := InitAdvertisement(data)
-	if adv.Rssi != 100 {
-		t.Fatalf("RSSI should be 100")
-	}
 	if len(adv.Structures) != 1 {
 		t.Fatalf("Length of structures does not equal 1")
 	}
+	if !bytes.Equal(adv.Structures[0].Data, []byte{0x00, 0x00, 0x00}) {
+		t.Fatalf("Short message does not match test data")
+	}
+
 }
