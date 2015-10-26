9f8e5a9812b10754a5c01cc4cfcca39292c4cd62
Jeffrey Meyers <jeffrey.alan.meyersj@gmail.com>
2015-10-26 09:28:14 -0700

    update from server

--- testbed/go-redis/basic.go
+++ testbed/go-redis/basic.go
@@ -32,6 +32,6 @@ func get(client *redis.Client, key string) string {
 
 func main() {
 	client := NewClient()
-	r := client.HGet("client:00000000-52b6-7b59-a10f-2aae09d71f21", "last_active")
-	fmt.Println(r)
+	client.Set("hello", "world", 0)
+	//r := client.HGet("client:00000000-52b6-7b59-a10f-2aae09d71f21", "last_active")
 }
