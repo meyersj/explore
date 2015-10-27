fdd29d9d317167c462c6c450fe3306b6fde61562
Jeffrey Meyers <meyers2@pdx.edu>
2015-10-27 15:11:07 -0700

    object used for nearby beacon data

---
+++ android-client/app/src/main/java/com/meyersj/tracker/NearbyBeacon.java
@@ -0,0 +1,51 @@
+package com.meyersj.tracker;
+
+import java.nio.ByteBuffer;
+
+public class NearbyBeacon {
+    public String hash;
+    public Integer count;
+    public Integer rssi;
+    public byte[] advertisement;
+    public boolean displayed = false;
+    public Double lat;
+    public Double lon;
+    public boolean registered;
+
+
+    public NearbyBeacon(byte[] advertisement, String hash, Integer rssi) {
+        this.advertisement = advertisement;
+        this.hash = hash;
+        this.rssi = rssi;
+        this.count = 0;
+        this.registered = false;
+    }
+
+    public NearbyBeacon(boolean registered, byte[] advertisement, String hash, Integer rssi) {
+        this.advertisement = advertisement;
+        this.hash = hash;
+        this.rssi = rssi;
+        this.count = 0;
+        this.registered = registered;
+    }
+
+    private byte[] doubleByteArray(double value) {
+        byte[] bytes = new byte[8];
+        ByteBuffer.wrap(bytes).putDouble(value);
+        return bytes;
+    }
+
+    public byte[] getLatitudeBytes() {
+        if (lat != null) {
+            return doubleByteArray(lat);
+        }
+        return doubleByteArray(0);
+    }
+
+    public byte[] getLongitudeBytes() {
+        if (lon != null) {
+            return doubleByteArray(lon);
+        }
+        return doubleByteArray(0);
+    }
+}
