d84c0fb8156436c6a91b3c37d2b8c306cd8a35aa
Jeffrey Meyers <meyers2@pdx.edu>
2015-10-27 15:09:21 -0700

    update utilities and add constant

---
+++ android-client/app/src/main/java/com/meyersj/tracker/Cons.java
@@ -0,0 +1,11 @@
+package com.meyersj.tracker;
+
+
+public class Cons {
+
+    public static final String SETTINGS_ACTIVITY = "com.meyersj.tracker.SETTINGS";
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String CONFIG_FILE = "config.properties";
+
+}
--- android-client/app/src/main/java/com/meyersj/tracker/Utils.java
+++ android-client/app/src/main/java/com/meyersj/tracker/Utils.java
@@ -2,22 +2,21 @@ package com.meyersj.tracker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.view.inputmethod.InputMethodManager;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.Socket;
 import java.util.Properties;
 import java.util.UUID;
 
-/**
- * Created by jeff on 10/14/15.
- */
+
 public class Utils {
 
     //read properties from config file in assets
-    public static Properties getProperties(Context context, String filename) {
+    private static Properties getProperties(Context context, String filename) {
         Properties properties = null;
 
         try {
@@ -27,6 +26,28 @@ public class Utils {
         } catch (IOException e) {
         }
         return properties;
+    }
+
+    public static String getHost(Context context) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String host = sharedPref.getString(Cons.HOST, "");
+        if (host.isEmpty()) {
+            Properties properties = getProperties(context, Cons.CONFIG_FILE);
+            host = properties.getProperty(Cons.HOST);
+            sharedPref.edit().putString(Cons.HOST, host).apply();
+        }
+        return host;
+    }
+
+    public static Integer getPort(Context context) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String port = sharedPref.getString(Cons.PORT, "");
+        if (port.isEmpty()) {
+            Properties properties = getProperties(context, Cons.CONFIG_FILE);
+            port = properties.getProperty(Cons.PORT);
+            sharedPref.edit().putString(Cons.PORT, port).apply();
+        }
+        return Integer.parseInt(port);
     }
 
     // return hex string for array of bytes
@@ -53,13 +74,6 @@ public class Utils {
     public static void hideKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
-    }
-
-    public static Socket openSocket(Context context) throws IOException {
-        Properties properties = Utils.getProperties(context, "config.properties");
-        String host = properties.getProperty("Host");
-        Integer port = Integer.valueOf(properties.getProperty("Port"));
-        return new Socket(host, port);
     }
 
 }
