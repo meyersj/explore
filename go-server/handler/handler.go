57e20a3881f7369ef097f370120894f81e36be35
Jeffrey Meyers <meyers2@pdx.edu>
2015-10-28 14:50:49 -0700

    handler changes

--- go-server/handler/handler.go
+++ go-server/handler/handler.go
@@ -3,36 +3,17 @@ package handler
 import (
 	"../data"
 	"../payload"
-	"crypto/sha1"
-	"encoding/binary"
 	"fmt"
-	"math"
-	"strconv"
 )
 
-func GetCoordinates(lat []byte, lon []byte) string {
-	lat_float := math.Float64frombits(binary.BigEndian.Uint64(lat))
-	lon_float := math.Float64frombits(binary.BigEndian.Uint64(lon))
-	lat_string := strconv.FormatFloat(lat_float, 'f', 5, 64)
-	lon_string := strconv.FormatFloat(lon_float, 'f', 5, 64)
-	return lat_string + " " + lon_string
-}
-
-func generate_beacon_key(advertisement []byte) string {
-	hash := sha1.Sum(advertisement)
-	hex := fmt.Sprintf("%0x", hash)
-	return "beacon:" + hex
-}
-
 func RegisterBeacon(p *payload.Payload) []byte {
-	fmt.Println("REGISTER BEACON")
 	message := payload.InitMessage(p.Data)
 	if len(message.Structures) == 3 {
 		name := string(message.Structures[0])
-		key := generate_beacon_key(message.Structures[2])
+		key := data.BuildBeaconKey(message.Structures[2])
 		lat := message.Structures[1][0:8]
 		lon := message.Structures[1][8:16]
-		coordinates := GetCoordinates(lat, lon)
+		coordinates := data.BuildCoordinates(lat, lon)
 		client := data.InitClient()
 		client.RegisterBeacon(key, name, coordinates)
 		fmt.Println("REGISTER BEACON", key, name, coordinates)
@@ -45,7 +26,7 @@ func RegisterBeacon(p *payload.Payload) []byte {
 func RegisterClient(p *payload.Payload) []byte {
 	message := payload.InitMessage(p.Data)
 	if len(message.Structures) == 2 {
-		device := "client:" + string(message.Structures[0])
+		device := data.BuildClientKey(message.Structures[0])
 		name := string(message.Structures[1])
 		client := data.InitClient()
 		client.RegisterClient(device, name)
@@ -60,8 +41,8 @@ func ClientUpdate(p *payload.Payload) []byte {
 	message := payload.InitMessage(p.Data)
 	if len(message.Structures) == 3 {
 		rssi := int8(message.Structures[0][0])
-		device := "client:" + string(message.Structures[1])
-		key := generate_beacon_key(message.Structures[2])
+		device := data.BuildClientKey(message.Structures[1])
+		key := data.BuildBeaconKey(message.Structures[2])
 		client := data.InitClient()
 		update := &data.ClientUpdate{Device: device, Beacon: key, Rssi: int(rssi)}
 		flag, data := client.ClientUpdate(update)
@@ -76,9 +57,9 @@ func ClientUpdate(p *payload.Payload) []byte {
 func PutMessage(p *payload.Payload) []byte {
 	message := payload.InitMessage(p.Data)
 	if len(message.Structures) == 3 {
-		device := "client:" + string(message.Structures[0])
+		device := data.BuildClientKey(message.Structures[0])
 		client_message := string(message.Structures[1])
-		key := generate_beacon_key(message.Structures[2])
+		key := data.BuildBeaconKey(message.Structures[2])
 		fmt.Println("\n"+device, key, client_message, "\n")
 		client := data.InitClient()
 		msg := &data.ClientMessage{Device: device, Beacon: key, Message: client_message}
@@ -87,5 +68,4 @@ func PutMessage(p *payload.Payload) []byte {
 		return payload.Build(0x00, []byte(display))
 	}
 	return payload.Build(0x01, []byte("error"))
-
 }
