1be441be5a6b2abc603240fe7bdc06f9546e1638
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-05 11:06:08 -0800

    remove old config param

--- go-server/server/conf.go
+++ go-server/server/conf.go
@@ -7,13 +7,11 @@ import (
 
 const DEFAULT_HOST string = "127.0.0.1"
 const DEFAULT_PORT string = "8082"
-const DEFAULT_UID string = "82C816B8CB37D896830F"
 const DEFAULT_REDIS string = "127.0.0.1:6379"
 
 type Config struct {
 	Host  string
 	Port  string
-	Uid   string
 	Redis string
 }
 
@@ -23,12 +21,11 @@ func Read_config(filename string) *Config {
 	if _, err := toml.DecodeFile(filename, &conf); err != nil {
 		conf.Host = DEFAULT_HOST
 		conf.Port = DEFAULT_PORT
-		conf.Uid = DEFAULT_UID
 		conf.Redis = DEFAULT_REDIS
-		fmt.Println("Error processing", filename)
-		fmt.Println("Default Host =", conf.Host)
-		fmt.Println("Default Port =", conf.Port)
-		fmt.Println("Default UID =", conf.Uid)
+		fmt.Println("Failure processing config file: ", filename)
+		fmt.Println("  default Host =", conf.Host)
+		fmt.Println("  default Port =", conf.Port)
+		fmt.Println("  default Redis =", conf.Redis)
 	}
 	return &conf
 }
