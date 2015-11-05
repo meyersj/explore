65c4423119ad797ec3c6d259a9a686aa8406fb07
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-05 11:12:42 -0800

    remove host param

--- go-server/server/conf.go
+++ go-server/server/conf.go
@@ -5,12 +5,10 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-const DEFAULT_HOST string = "127.0.0.1"
 const DEFAULT_PORT string = "8082"
 const DEFAULT_REDIS string = "127.0.0.1:6379"
 
 type Config struct {
-	Host  string
 	Port  string
 	Redis string
 }
@@ -19,11 +17,9 @@ type Config struct {
 func Read_config(filename string) *Config {
 	var conf Config
 	if _, err := toml.DecodeFile(filename, &conf); err != nil {
-		conf.Host = DEFAULT_HOST
 		conf.Port = DEFAULT_PORT
 		conf.Redis = DEFAULT_REDIS
 		fmt.Println("Failure processing config file: ", filename)
-		fmt.Println("  default Host =", conf.Host)
 		fmt.Println("  default Port =", conf.Port)
 		fmt.Println("  default Redis =", conf.Redis)
 	}
